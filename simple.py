73f5b083fc00708bb93258a6a24d8c35e73b53ca
R0Pasha <architut.by@gmail.com>
2022-07-22 12:13:39 +0300

    пробная попытка вывода чисел, файл в main 12

--- simple.py
+++ simple.py
@@ -1,6 +1,7 @@
-# coding = UTF-8 
-x = int(input("Введите первое  число:"))
-y = int(input("Введите второе  число:"))
+#!/usr/bin/python
+# -*- coding: UTF-8 -*- 
+x = int(input("Введите первое число:"))
+y = int(input("Введите второе число:"))
 numbers = [x,y]
 numbers.sort (reverse=True)
 while numbers[0]>=numbers[1]:
